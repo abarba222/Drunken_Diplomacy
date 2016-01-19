b493b7804fbf7ee1153f20b2ad65293aa5d5f542
Anthony Barba <abarba222@gmail.com>
2016-01-19 05:04:02 -0500

    added files to drunken folder

---
+++ scripts/events.js
@@ -0,0 +1,7 @@
+window.Game = window.Game || {};
+
+  Game.vent = _.extend({}, Backbone.Events);
+
+  // Game.vent.on('choose:heroes', function(heroes) {
+  // $('.leader-buttons').html(JST['index'](heroes));
+  // });
